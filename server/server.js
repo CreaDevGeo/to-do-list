e42576ffe5049fe5ad14c256937f715e1a94c9db
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-06-25 13:04:04 -0500

    All server code was missing on main branch so, copied and pasted from new-feature branch. Everything should be god to go? Idek at this point.

---
+++ server/server.js
@@ -0,0 +1,30 @@
+// Imports and Routers
+// Express
+const express = require('express');
+
+// Express function
+const app = express();
+
+// Body parser
+const bodyParser = require('body-parser');
+
+// Port
+const port = 5000;
+
+// Router: '/task'
+let taskRouter = require('./routes/task.js');
+
+
+// Configurations
+app.use(express.static('public/'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Request to this url will route to taskRouter
+app.use('/task', taskRouter);
+
+
+
+// Server start
+app.listen(port, () => {
+    console.log("Up and running on port:", port);
+}); // end server start
