3f101eede6ee164b7c93221eb613557f86dd3dab
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-06-26 10:10:39 -0500

    Updated SQL in databse.sql file. Should now match sql in 'weekend-to-do-app' database.

--- database.sql
+++ database.sql
@@ -2,6 +2,9 @@ CREATE TABLE "to-do-list" (
   "id" SERIAL PRIMARY KEY,
   "task" VARCHAR(80) NOT NULL,
   "priority" INTEGER CHECK ("priority" >= 1 AND "priority" <= 3),
-  "end-date" DATE,
-  "completed" BOOLEAN DEFAULT FALSE NOT NULL
-);
+  "due_date" DATE,
+  "completion" BOOLEAN DEFAULT FALSE);
+
+-- DROP TABLE "to-do-list";
+
+SELECT * FROM "to-do-list" ORDER BY "priority" ASC;
