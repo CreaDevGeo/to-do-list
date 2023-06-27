67535be56a8f54fb6964699b0674d28132cc78bd
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-06-26 23:25:29 -0500

    Changed my PORT variable name in other areas of server.js.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// Imports and Routers
+// ImPORTs and Routers
 // Express
 const express = require('express');
 
@@ -8,7 +8,7 @@ const app = express();
 // Body parser
 const bodyParser = require('body-parser');
 
-// Port
+// PORT
 const PORT = process.env.PORT || 5000;
 
 // Router: '/task'
@@ -25,6 +25,6 @@ app.use('/task', taskRouter);
 
 
 // Server start
-app.listen(port, () => {
-    console.log("Up and running on port:", port);
+app.listen(PORT, () => {
+    console.log("Up and running on PORT:", PORT);
 }); // end server start
